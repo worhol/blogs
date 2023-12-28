2582a8f841c5d2bf9fd6ae1545353cebef890d09
worhol <trashuries@gmail.com>
2023-12-28 10:24:15 -0600

    Completed all tests, added delete and put http methods

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,15 +6,40 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', async(request, response, next) => {
-  const blog = await new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-    .catch((error) => next(error))
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0
+  })
+  if (body.title && body.url) {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } else {
+    response.status(400).end()
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true
+  })
+  response.status(204).json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -50,6 +50,63 @@ test('post request creates a new blog post', async () => {
   expect(title).toContain('Architecture in web')
 })
 
+test('if likes is missing defaluts to 0', async () => {
+  const newBlog = {
+    title: 'Architecture in web',
+    author: 'Robert',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html'
+  }
+  await api.post('/api/blogs').send(newBlog).expect(201)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const lastBlog = blogsAtEnd[helper.initialBlogs.length]
+  expect(lastBlog.likes).toBe(0)
+})
+test('backend responds with code 400 if the title is missing', async () => {
+  const newBlog = {
+    author: 'Robert',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 5
+  }
+  await api.post('/api/blogs').send(newBlog).expect(400)
+})
+test('backend responds with code 400 if the url is missing', async () => {
+  const newBlog = {
+    author: 'Robert',
+    title: 'Good web development'
+  }
+  await api.post('/api/blogs').send(newBlog).expect(400)
+})
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+  const titles = blogsAtEnd.map((blog) => blog.title)
+  expect(titles).not.toContain(blogToDelete.title)
+
+  expect()
+})
+test('a blog can be updated', async () => {
+  const blogAtStart = await helper.blogsInDb()
+  const blogtoUpdate = blogAtStart[0]
+  const updatedBlogData = {
+    title: blogtoUpdate.title,
+    author: blogtoUpdate.author,
+    url: blogtoUpdate.url,
+    likes: blogtoUpdate.likes + 1
+  }
+  await api
+    .put(`/api/blogs/${blogtoUpdate.id}`)
+    .send(updatedBlogData)
+    .expect(204)
+  const blogsAtEnd = await helper.blogsInDb()
+  const updatedBlog = blogsAtEnd[0]
+  expect(updatedBlog.likes).toBe(blogtoUpdate.likes + 1)
+})
 afterAll(async () => {
   await mongoose.connection.close()
 })
