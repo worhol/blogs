39622a28369b058b4a49bb976919e6ad30aad0fd
worhol <trashuries@gmail.com>
2024-01-09 14:06:35 -0600

    Redefined PropTypes for CreateBlogForm and Togglable

--- bloglist-frontend/src/components/Togglable.jsx
+++ bloglist-frontend/src/components/Togglable.jsx
@@ -1,4 +1,5 @@
 import { useState, forwardRef, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
 
 const Togglable = forwardRef((props, refs) => {
   const [visible, setVisible] = useState(false)
@@ -10,9 +11,9 @@ const Togglable = forwardRef((props, refs) => {
     setVisible(!visible)
   }
 
-  useImperativeHandle(refs, ()=>{
-    return{
-        toggleVisibility
+  useImperativeHandle(refs, () => {
+    return {
+      toggleVisibility
     }
   })
 
@@ -28,5 +29,7 @@ const Togglable = forwardRef((props, refs) => {
     </div>
   )
 })
-
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
 export default Togglable
