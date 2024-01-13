eaf8aeee7fe5176206ec893c145e2dc2d96cb9b1
worhol <trashuries@gmail.com>
2024-01-13 10:25:39 -0600

    added tests for successful and unsuccessful login attempts. test checks that notification shown with unsuccessful login is displayed red.

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      username: 'john',
+      name: 'John',
+      password: 'pass'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:5173')
   })
 
@@ -9,5 +15,22 @@ describe('Blog app', function () {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('john')
+      cy.get('#password').type('pass')
+      cy.get('#login').click()
+      cy.contains('John logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('john')
+      cy.get('#password').type('passw')
+      cy.get('#login').click()
+      cy.get('.error')
+        .contains('invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -118,7 +118,9 @@ const App = () => {
   }
 
   const deleteBlog = async (blogToDelete) => {
-    const confirm = window.confirm(`Remove blog ${blogToDelete.title} by ${blogToDelete.author} ?`)
+    const confirm = window.confirm(
+      `Remove blog ${blogToDelete.title} by ${blogToDelete.author} ?`
+    )
     if (confirm) {
       try {
         await blogService.remove(blogToDelete)
@@ -143,6 +145,7 @@ const App = () => {
           <div>
             username
             <input
+              id="username"
               type="text"
               value={username}
               name="Username"
@@ -152,13 +155,14 @@ const App = () => {
           <div>
             password
             <input
+              id="password"
               type="password"
               value={password}
               name="Password"
               onChange={({ target }) => setPassword(target.value)}
             />
           </div>
-          <button type="submit">login</button>
+          <button id='login' type="submit">login</button>
         </form>
       </div>
     )
